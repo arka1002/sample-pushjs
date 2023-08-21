e61d7cc47c76218b0a660d7442d99313ae8a61f5
arka1002 <arka.mcu700@gmail.com>
2023-08-21 15:36:34 +0530

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <button @click="increment">+1</button>
+    <p>{{ counter }}</p>
+    <div v-bind:class="styles.cont">
+      <button @click="createNotif">Create</button>
+      <button @click="closeNotif">Close</button>
+    </div>
+  </div>
+</template>
+<script setup>
+import Push from "push.js";
+
+const counter = useState("counter", () => 5);
+
+function increment() {
+  counter.value++;
+}
+
+function createNotif() {
+  Push.create("Simple Notif", {
+    body: "This is a simple notification.",
+    timeout: 5000,
+    tag: "foo",
+  });
+}
+function closeNotif() {
+  Push.close("foo");
+}
+</script>
+
+<style module="styles">
+.cont {
+  display: flex;
+  gap: 10px;
+  align-items: center;
+}
+</style>
